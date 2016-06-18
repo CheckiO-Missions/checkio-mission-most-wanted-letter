a734e1d1fc08613935e01b64de07c3de1043c4de
oduvan <a.lyabah@checkio.org>
2016-06-18 16:47:57 +0300

    ext_editor_io

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -1,137 +1,11 @@
 //Dont change it
-requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
-    function (ext, $, TableComponent) {
-
-        var cur_slide = {};
-
-        ext.set_start_game(function (this_e) {
-        });
-
-        ext.set_process_in(function (this_e, data) {
-            cur_slide["in"] = data[0];
-        });
-
-        ext.set_process_out(function (this_e, data) {
-            cur_slide["out"] = data[0];
-        });
-
-        ext.set_process_ext(function (this_e, data) {
-            cur_slide.ext = data;
-            this_e.addAnimationSlide(cur_slide);
-            cur_slide = {};
-        });
-
-        ext.set_process_err(function (this_e, data) {
-            cur_slide['error'] = data[0];
-            this_e.addAnimationSlide(cur_slide);
-            cur_slide = {};
-        });
-
-        ext.set_animate_success_slide(function (this_e, options) {
-            var $h = $(this_e.setHtmlSlide('<div class="animation-success"><div></div></div>'));
-            this_e.setAnimationHeight(115);
-        });
-
-        ext.set_animate_slide(function (this_e, data, options) {
-            var $content = $(this_e.setHtmlSlide(ext.get_template('animation'))).find('.animation-content');
-            if (!data) {
-                console.log("data is undefined");
-                return false;
-            }
-
-
-            if (data.error) {
-                $content.find('.call').html('Fail: checkio(' + ext.JSON.encode(data.in) + ')');
-                $content.find('.output').html(data.error.replace(/\n/g, ","));
-
-                $content.find('.output').addClass('error');
-                $content.find('.call').addClass('error');
-                $content.find('.answer').remove();
-                $content.find('.explanation').remove();
-                this_e.setAnimationHeight($content.height() + 60);
-                return false;
-            }
-
-            var checkioInput = data.in;
-            var rightResult = data.ext["answer"];
-            var userResult = data.out;
-            var result = data.ext["result"];
-            var result_addon = data.ext["result_addon"];
-
-            $content.find('.call').html('Fail: checkio(' + ext.JSON.encode(data.in) + ')');
-
-
-            //if you need additional info from tests (if exists)
-
-            $content.find('.output').html('&nbsp;Your result:&nbsp;' + ext.JSON.encode(userResult));
-            $content.find('.call').remove();
-
-            if (!result) {
-//                $content.find('.call').html('Fail: checkio(' + ext.JSON.encode(checkioInput) + ')');
-                $content.find('.answer').html('Right result:&nbsp;' + ext.JSON.encode(rightResult));
-                $content.find('.answer').addClass('error');
-                $content.find('.output').addClass('error');
-                $content.find('.call').addClass('error');
-            }
-            else {
-//                $content.find('.call').html('Pass: checkio(' + ext.JSON.encode(checkioInput) + ')');
-                $content.find('.answer').remove();
-
-            }
-            //Dont change the code before it
-
-            var canvas = new MWLAnimation();
-            canvas.createExplanation($content.find(".explanation"), checkioInput, rightResult, true);
-
-
-            this_e.setAnimationHeight($content.height() + 60);
-
-        });
-
-
+requirejs(['ext_editor_io', 'jquery_190', 'raphael_210'],
+    function (extIO, $, TableComponent) {
         var $tryit;
         var tCanvas;
         var inText;
         var $textInput;
         var $textResult;
-
-        ext.set_console_process_ret(function (this_e, ret) {
-            if (typeof(ret) === 'string') {
-                ret = ret.replace(/\'/g, "");
-            }
-            var htmlText;
-            if (typeof ret === "string" && ret.length === 1) {
-                htmlText = tCanvas.processText(inText, ret);
-            }
-            else {
-                htmlText = inText;
-            }
-            $textResult.html(htmlText);
-            console.log(htmlText);
-            $textResult.removeClass("invisible");
-            $textInput.addClass("invisible");
-            $tryit.find(".checkio-result").html("Result<br>" + JSON.stringify(ret));
-        });
-
-        ext.set_generate_animation_panel(function (this_e) {
-
-            $tryit = $(this_e.setHtmlTryIt(ext.get_template('tryit')));
-            tCanvas = new MWLAnimation();
-            $textInput = $tryit.find(".text-input");
-            $textResult = $tryit.find(".text-result");
-
-            $textResult.click(function () {
-                $textResult.toggleClass("invisible");
-                $textInput.toggleClass("invisible");
-            });
-
-            $tryit.find(".bn-check").click(function (e) {
-                inText = $textInput.val();
-                this_e.sendToConsoleCheckiO(inText);
-                e.stopPropagation();
-                return false;
-            });
-        });
 
         function MWLAnimation() {
             var colorOrange4 = "#F0801A";
@@ -178,15 +52,57 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
                     }
                 }
                 return htmlText;
-            }
+            };
         }
 
 
-        //Your Additional functions or objects inside scope
-        //
-        //
-        //
+        var io = new extIO({
+            animation: function($expl, data){
+                var checkioInput = data.in;
+                var rightResult = data.ext?data.ext.answer:undefined;
 
+                if (!checkioInput || !rightResult){
+                    return;
+                }
+                var canvas = new MWLAnimation();
+                canvas.createExplanation($content.find(".explanation"), checkioInput, rightResult, true);
+                this_e.setAnimationHeight($content.height() + 60);
+            },
+            tryit: function() {
+                $tryit = this_e.extSetHtmlTryIt(this_e.getTemplate('tryit'));
+                tCanvas = new MWLAnimation();
+                $textInput = $tryit.find(".text-input");
+                $textResult = $tryit.find(".text-result");
 
+                $textResult.click(function () {
+                    $textResult.toggleClass("invisible");
+                    $textInput.toggleClass("invisible");
+                });
+
+                $tryit.find(".bn-check").click(function (e) {
+                    inText = $textInput.val();
+                    this_e.extSendToConsoleCheckiO(inText);
+                    e.stopPropagation();
+                    return false;
+                });
+            },
+            retConsole:function (ret) {
+                if (typeof(ret) === 'string') {
+                    ret = ret.replace(/\'/g, "");
+                }
+                var htmlText;
+                if (typeof ret === "string" && ret.length === 1) {
+                    htmlText = tCanvas.processText(inText, ret);
+                }
+                else {
+                    htmlText = inText;
+                }
+                $textResult.html(htmlText);
+                $textResult.removeClass("invisible");
+                $textInput.addClass("invisible");
+                $tryit.find(".checkio-result").html("Result<br>" + JSON.stringify(ret));
+            }
+        });
+        io.start();
     }
 );
