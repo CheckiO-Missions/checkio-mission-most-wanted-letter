3142582b2b191424d9d801184712bea5af5d5db8
Oleksandr Zozulia <freemanlex@gmail.com>
2022-08-08 23:33:39 +0300

    next-api hints uk hints

--- verification/referee.py
+++ verification/referee.py
@@ -1,14 +1,21 @@
 from checkio.signals import ON_CONNECT
 from checkio import api
-from checkio.referees.io import CheckiOReferee
+from checkio.referees.io_template import CheckiOReferee
 
 from tests import TESTS
 
 api.add_listener(
     ON_CONNECT,
     CheckiOReferee(
-    	tests=TESTS,
-    	function_name={
-            "python": "checkio",
-            "js": "mostWanted"
-        }).on_ready)
+        tests=TESTS,
+        function_name={
+            "python": "checkio", # check function name!
+            "js": "mostWanted"      # check function name!
+        },
+        cover_code={
+            'python-3': {},
+            'js-node': {
+                # "dateForZeros": True,
+            }
+        }
+    ).on_ready)
